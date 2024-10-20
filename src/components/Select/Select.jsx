008915f3e4b0d2d16e8244b0102e86b69b515a95
Heloisa Cativo <heloisacativo@gmail.com>
2024-10-20 17:44:11 -0400

    add: select component

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,31 @@
+export const Select = ({ placeholder, value, onChange, className="", options = []}) => {
+    return (
+        <select
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`
+        flex
+        bg-dark-violet-background
+        text-blue-normal-light
+        border-blue-normal
+        border-b-2 
+        border-t-2 
+        border-r-2 
+        border-l-2 
+        outline-none
+       placeholder:text-blue-normal-light
+        placeholder:opacity-50 
+        focus:border-stone-600 
+        rounded-md mb-5 ${className}`}
+        >
+        {options.map((option) => (
+            <option key={option.value} value={option.value}>
+                {option.label}
+            </option>
+        ))}
+        </select>
+    )
+}
+
+export default Select;
