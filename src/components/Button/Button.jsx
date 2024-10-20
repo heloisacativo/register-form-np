54756a623a4706c4727a2e8206fd7871a5c8469e
Heloisa Cativo <heloisacativo@gmail.com>
2024-10-20 17:42:44 -0400

    add: button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,22 @@
+export const Button = ({type,  color = 'bg-blue-normal hover:bg-stone-800', size = 'medium',  rounded = '', text = "", className ="", title, onClick, children }) => {
+
+    const sizeClasses = {
+         small: 'px-4 py-2 text-sm',
+         medium: 'px-6 py-3 text-base',
+         large: 'px-8 py-3 text-lg'
+    }
+    return (
+        <button type={type}
+        className={`${color}  ${text} text-sky-00 border-none outline-offset-0 hover:outline-gray-500 outline-none hover:duration-200 ${sizeClasses[size]} ${className} ${rounded}`}
+        title={title}
+        onClick={onClick}
+        >
+        {children}
+        </button>
+
+
+    )
+}
+
+
+export default Button
