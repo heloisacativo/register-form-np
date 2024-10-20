f5f846ca80924dc5dc22367992f6dd87e16ccab2
Heloisa Cativo <heloisacativo@gmail.com>
2024-10-20 17:46:01 -0400

    props

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,18 +1,23 @@
 import { Select } from "../Select/Select.jsx"
 
-export const Input = ({placeholder ="", value, onChange, type="text", label, className = "", options = [],}) => {
-    const defaultClass = "flex bg-dark-violet-background text-blue-normal border-blue-normal text-center border-b-2 border-t-2 border-r-2 border-l-2 px-28 py-3 outline-none placeholder:text-blue-normal-light placeholder:opacity-50 focus:border-stone-600 invalid:focus:border-red-400 valid:focus:border-green-400 rounded-md mb-5"
+export const Input = ({placeholdercolor = "", placeholder ="", size = "medium", value, onChange, type="text", label, className = "", options = [],}) => {
+    const sizeClasses = {
+        small: 'xxs:py-1 px-2 py-2 w-full text-sm',
+        medium: 'xxs:py-1 px-2 py-1 w-full text-base',
+        large: 'xxs:py-1 px-2 py-3 w-full text-lg'
+   }
+    
     return (
-        <div className="inputContainer">
+        <div>
                 <label className="text-blue-normal">{label}</label>
                 {type === "select" ? (
-                    <Select value={value} onChange={onChange} className={className} options={options}/>
+                    <Select value={value} onChange={onChange} className={` ${sizeClasses[size]} ${className}`} options={options}/>
                 ): (
                 <input type={type}
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
-                className={` ${className ? className : defaultClass}`}
+                className={` ${placeholder} ${placeholdercolor} ${sizeClasses[size]} flex bg-dark-violet-background text-blue-normal border-blue-normal text-start border-b-2 border-t-2 border-r-2 border-l-2 px-1 py-4 outline-none placeholder:text-blue-normal-light placeholder:opacity-50 focus:border-stone-600 invalid:focus:border-red-400 valid:focus:border-green-400 rounded-md mb-5 w-96`}
                />
                )}
         </div>
