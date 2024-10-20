fdde157f2f8fe75cc0d251837f7e28ef9b08191f
Heloisa Cativo <heloisacativo@gmail.com>
2024-10-20 00:32:31 -0400

    base

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ 
+    "./index.html",
+    "./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      'dark-violet-background': '#030712',
+      'blue-light-text': '#8A92A4',
+      'blue-normal': '#225E86',
+      'blue-normal-light': '#25323A',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
