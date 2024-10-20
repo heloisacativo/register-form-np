c18a01b83e6ce15266f3ce393b5ad5bfe20dbe2f
Heloisa Cativo <heloisacativo@gmail.com>
2024-10-20 17:56:28 -0400

    add screen sizes and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,20 @@ export default {
       'blue-light-text': '#8A92A4',
       'blue-normal': '#225E86',
       'blue-normal-light': '#25323A',
+      'blue-light': '#ADADDF',
+      'stormy-blue': '#476A98',
+      'stormy-blue-soft': '#84B4D4'
+    },
+    screens: {
+      xxs: '360px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {},
   },
   plugins: [],
 }
-
