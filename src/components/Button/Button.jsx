fc590bb0242d35634f7b018ea33d332262f7afb5
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-10-23 21:21:04 -0400

    feat(Home):add request form data

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,31 @@
-export const Button = ({type,  color = 'bg-blue-normal hover:bg-stone-800', size = 'medium',  rounded = '', text = "", className ="", title, onClick, children }) => {
+import React from 'react';
 
+export const Button = ({
+    type,
+    color = 'bg-blue-normal hover:bg-stone-800',
+    size = 'medium',
+    rounded = '',
+    text = "",
+    className = "",
+    title,
+    onClick,
+    children
+}) => {
     const sizeClasses = {
-         small: 'px-4 py-2 text-sm',
-         medium: 'px-6 py-3 text-base',
-         large: 'xxs:py-2 xxs:px-6 px-8 py-3 text-lg'
-    }
+        small: 'px-4 py-2 text-sm',
+        medium: 'px-6 py-3 text-base',
+        large: 'xxs:py-2 xxs:px-6 px-8 py-3 text-lg'
+    };
     return (
-        <button type={type}
-        className={`${color}  ${text} text-sky-00 border-none outline-offset-0 hover:outline-gray-500 outline-none hover:duration-200 ${sizeClasses[size]} ${className} ${rounded}`}
-        title={title}
-        onClick={onClick}
+        <button
+            type={type}
+            className={`${color} ${text} text-sky-00 border-none outline-offset-0 hover:outline-gray-500 outline-none hover:duration-200 ${sizeClasses[size]} ${className} ${rounded}`}
+            title={title}
+            onClick={onClick}
         >
-        {children}
+            {children}
         </button>
+    );
+};
 
-
-    )
-}
-
-
-export default Button
+export default Button;
