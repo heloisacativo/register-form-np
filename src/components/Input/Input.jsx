85420cbb86d537b0c2560447b128f97b82f33fd0
heloisacativo <heloisacativo@gmail.com>
2024-11-17 15:35:21 -0400

    add: eyes slash icon in password input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,4 @@
-import { PhotoIcon } from '@heroicons/react/16/solid';
+import { EyeIcon, EyeSlashIcon, PhotoIcon } from '@heroicons/react/16/solid';
 import React, { useState } from 'react';
 
 export const Input = ({
@@ -20,7 +20,7 @@ export const Input = ({
     const sizeClasses = {
         small: 'xxs:py-1 px-2 py-2 w-full text-sm',
         medium: 'xxs:py-1 px-2 py-1 w-full text-base',
-        large: 'xxs:py-2 px-2 py-3 w-full text-lg'
+        large: 'xxs:py-1 px-2 py-2 w-full text-md'
     };
 
     const handleClick = () => {
@@ -33,7 +33,7 @@ export const Input = ({
 
     return (
         <div>
-            <label className="text-blue-normal">{label}</label>
+            <label className="text-muted-foreground">{label}</label>
             {type === "file" ? (
                 <div className="flex flex-col items-start">
                 <input
@@ -46,7 +46,7 @@ export const Input = ({
                 <button
                   type="button"
                   onClick={handleClick}
-                  className="text-blue-normal border-blue-normal border-2 py-2 px-4 rounded-md focus:ring-2 focus:ring-stone-600 focus:outline-none hover:bg-violet-800 hover:text-white w-full text-start flex items-center justify-between"
+                  className="mb-2 mt-1 bg-background text-muted-foreground border py-1 px-3 rounded-md focus:ring-2 focus:ring-stone-600 focus:outline-none w-full text-start flex items-center justify-between"
                 >
                   Selecionar Imagem
                   <PhotoIcon className="w-5 h-5 ml-2"/>
@@ -60,15 +60,19 @@ export const Input = ({
                     value={value}
                     onChange={onChange}
                     placeholder={placeholder}
-                    className={` ${placeholder} ${placeholdercolor} ${sizeClasses[size]} ${color} flex bg-dark-violet-background text-blue-normal border-blue-normal text-start border-b-2 border-t-2 border-r-2 border-l-2 px-1 py-4 outline-none placeholder:text-blue-normal-light placeholder:opacity-50 focus:border-stone-600 invalid:focus:border-red-400 valid:focus:border-green-400 rounded-md mb-5 w-96`}
+                    className={` ${placeholder} ${placeholdercolor} ${sizeClasses[size]} ${color} flex bg-background text-muted-foreground border-border border text-start outline-none placeholder:text-muted-foreground placeholder:opacity-50 invalid:focus:border-red-400 valid:focus:border-green-400 rounded-md mb-2 mt-1 w-96`}
                 />
                 {type === "password" && (
                     <button
                     type='button'
                     onClick={passwordVisibility}
-                    className="absolute right-2 transform -translate-y-1/2 bg-blue-normal text-dark-violet-background px-3 rounded-md focus:outline-none hover:bg-violet-800"
+                    className="absolute right-2 transform -translate-y-2/2 text-muted-foreground px-3 rounded-md focus:outline-noneborder-border"
                     >
-                    {showPassword ? "Ocultar" : "Mostrar"}
+                    {showPassword ? (
+                        <EyeSlashIcon className='h-5 w-5' aria-hidden="true"/>
+                    ) : (
+                        <EyeIcon className='h-5 w-5' aria-hidden="true"/>
+                    )}
                     </button>
             )}
             </div>
